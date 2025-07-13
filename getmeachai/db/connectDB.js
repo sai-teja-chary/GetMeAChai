760a127b6610ccc292a47ac6d3ba636cc5bb1168
Sai Teja Chary Thummalapally <saitejachary1411@gmail.com>
2025-07-13 13:40:00 +0530

    typo error

--- getmeachai/db/connectDB.js
+++ getmeachai/db/connectDB.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`{process.env.MONGODB_URI}/GetMeAChai`, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/GetMeAChai`)
     console.log(`MongoDB Connected: {conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
